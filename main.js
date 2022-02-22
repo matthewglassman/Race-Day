45116f227410030a03a76c0c4f0c71b287503402
Matthew Glassman <matthewglassman78@gmail.com>
2022-02-22 11:40:15 -0500

    Had to make the words true and false lowercase.

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 let raceNumber = Math.floor(Math.random()* 1000);
-let registeredEarly = True;
+let registeredEarly = true;
 let runnersAge = 21;
 
-if (runnersAge >= 18 && registeredEarly === True){
+if (runnersAge >= 18 && registeredEarly === true){
     raceNumber += 1000;
 }
 
 console.log(raceNumber);
 
-if (runnersAge >= 18 && registeredEarly === True){
+if (runnersAge >= 18 && registeredEarly === true){
     console.log(`Runner number ${raceNumber} you are racing at 9:30am`);
-} else if (runnersAge >= 18 && registeredEarly === False){
+} else if (runnersAge >= 18 && registeredEarly === false){
     console.log(`Runner number ${raceNumber}, you are racing at 11:00am`);
 } else {
     console.log(`Runner number ${raceNumber}, you are racing at 12:30pm`);
